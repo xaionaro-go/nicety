a554fb8831d67f6e32073716d30fee8e72351aaf
Dmitry Yu Okunev <xaionaro@wisebits.com>
2019-01-04 11:03:12 +0200

    Demonstrate also map element deletion

--- modifingMap/map.go
+++ modifingMap/map.go
@@ -1,20 +1,22 @@
 package main
 
 // $ go run map.go
-// map[oldValue:1 newValue:2]
+// map[oldValue:2 newValue:3]
 
 import (
 	"fmt"
 )
 
 func modifyMap(m map[string]int) {
-	m["oldValue"] = 1
-	m["newValue"] = 2
+	m["oldValue"] = 2
+	delete(m, "anotherOldValue")
+	m["newValue"] = 3
 }
 
 func main() {
 	m := map[string]int{}
 	m["oldValue"] = 0
+	m["anotherOldValue"] = 1
 	modifyMap(m)
 	fmt.Println(m)
 }
